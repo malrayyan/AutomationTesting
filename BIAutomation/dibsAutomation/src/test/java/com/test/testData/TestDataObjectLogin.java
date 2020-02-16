3f4fcd2eb874dcf14fa1c39e6e25ac8881e7d51a
Premc468 <Premc468@gmail.com>
2020-02-16 22:46:40 +0530

    removed dir

--- BIAutomation/dibsAutomation/src/test/java/com/test/testData/TestDataObjectLogin.java
+++
@@ -1,8 +0,0 @@
-package com.test.testData;
-
-public class TestDataObjectLogin {
-
-	public static String LOGIN_DETAILS = "";
-	public static String JOURNEY_ID = "";
-	public static String TEST_DESCRIPTION = "";
-}
--- BIAutomation/dibsAutomation/src/test/java/com/test/utils/ModuleTestData.java
+++
@@ -1,8 +0,0 @@
-package com.test.utils;
-
-public class ModuleTestData {
-
-	public static String JOURNEY_ID = "";
-	public static String PILOT_GROUP = "";
-	public static String TEST_DESCRIPTION = "";
-}
